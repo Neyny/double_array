8d7abf3d172fd07b6a6d29756a37885146c67fb3
Neyny <34395006+Neyny@users.noreply.github.com>
2018-02-04 12:57:09 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,7 @@ int main() {
 	int a[5][5];
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			if (j ==(4 - i)) {
-				a[i][j] = 7;
-			}
-			else {
-				a[i][j] = 0;
-			}
+				a[i][j] = rand() % 10;
 			cout << " " << a[i][j];
 		}
 		cout << endl;
